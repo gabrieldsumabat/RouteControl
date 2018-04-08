cf3391704fb215afacb65d924f4da8ce55161296
Gabe <gabrieldsumabat@gmail.com>
2018-04-08 19:50:46 -0400

    Major Completion, project in working state.
    Notes:
    1. Hop is RC between the target ASN and the source ASN.
    2. addressBook volatility ensures the latest updates are properly displayed.
    3. Ports are hardcoded to 1450
    
    Changes:
    1. Added Javadocs

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,9 @@
 import java.io.*;
 
-
+/**
+ * Initializes the Starting State of the Route Controller
+ * Populates the Address Book with the Immediate Neighbors
+ */
 public class Config {
     volatile   ASN myASN;
     private ASN[] externalRC = new ASN[25];
@@ -8,6 +11,24 @@ public class Config {
     private int nor;
     private volatile int noa;
 
+    /**
+     *  Populates myASN with local configuration values and Address Book with all known direct connections
+     *  Reads from file config.txt
+     *  First line: RCID ASNID IPA  //Of local ASN
+     *  Second line: R[Number of Known Directly Connected Route Controllers]
+     *  Third line to 3+R : RCID ASNID IPA //of each Directly Connected Route Controller
+     *  (3+R)+1 line: A [Number of directly connected ASN]
+     *  (3+R)+2 to EoF: ASNID LinkCapacity LinkCost
+     *
+     * EX:
+     * 1 100 192.168.2.179
+     * 1
+     * 2 200 192.168.2.17
+     * 3
+     * 10 2 5
+     * 20 5 5
+     * 200 10 500
+     */
     public Config() {
         try{
             FileInputStream fstream = new FileInputStream("config.txt");
@@ -17,7 +38,7 @@ public class Config {
             nor = Integer.parseInt(br.readLine());
             for (int i=0; i<nor; i++) {
                 splits = br.readLine().split(" ");
-                //Link Cost is initiated to 5000 to ensure the first setLinkCost will override the value.
+                //Link Cost is initiated to 500 to ensure the first setLinkCost will override the value.
                 externalRC[i] = new ASN(Integer.parseInt(splits[1]), 1, 500, Integer.parseInt(splits[0]), splits[2]);
             }
             noa = Integer.parseInt(br.readLine());
@@ -42,15 +63,25 @@ public class Config {
         }
     }
 
+    /**
+     * Returns the ASN ID containing the input Route Controller ID
+     * @param RCID ID of Route Controller
+     * @return ASNID if Route Controller exists in known ASN, else 0
+     */
     public int getASNfromRC(int RCID){
         for (int l=0; l<noa;l++){
             if (addressBook[l].getRCID() == RCID) {
                 return addressBook[l].getASNID();
             }
         }
-        return (-1);
+        return (0);
     }
 
+    /**
+     * Returns IP Address of ASN
+     * @param ASN ASN ID as an int
+     * @return IPA of ASN, if ASN does not exist it will return null
+     */
     public String getIPAfromASN(int ASN){
         for (int a=0;a<noa;a++){
             if (addressBook[a].getASNID()==ASN){
@@ -60,6 +91,11 @@ public class Config {
         return null;
     }
 
+    /**
+     * Checks if the ASN is known to Route Controller
+     * @param ASN ASNID of advertised network
+     * @return true if the ASN is already known, false if it does not exist in address book
+     */
     public Boolean ASNinBook(int ASN){
         for (int a=0;a<noa;a++){
             if (addressBook[a].getASNID()==ASN){
@@ -69,10 +105,18 @@ public class Config {
         return false;
     }
 
+    /**
+     * Returns Number of Addresses known to Local Route Controller
+     * @return int value representing number of known addresses
+     */
     public int getNoa() {
         return noa;
     }
 
+    /**
+     * Sets the Number of Addresses known to the Local Route Controller
+     * @param noadd int value of the new total number of addresses known
+     */
     public void setNoa(int noadd){
         noa = noadd;
     }
--- src/RCU.java
+++ src/RCU.java
@@ -2,6 +2,10 @@ import java.io.Serializable;
 import java.lang.System;
 import java.net.InetAddress;
 
+/**
+ * RCU Class is a serializable object to be communicated between Route Controllers through Socket connections.
+ * The RCU implements both Advertisement updates and RTT packets.
+ */
 
 public class RCU implements Serializable {
     //Serialized Object to be exchanged over Socket Connections
@@ -18,6 +22,15 @@ public class RCU implements Serializable {
     //Route Advertisement
     private ASN[] advertisement;
 
+    /**
+     *  Creates new RCU Packet
+     * @param Source RCID of Source ASN
+     * @param NextHop ASNID of target ASN
+     * @param HopType 2 if target ASN has a RCID, 1 if ASN has no RCID
+     * @param Cost Sender's Link Cost
+     * @param RTT_Update RTT Flag, 1 for RTT_REQ and 0 for RCU Advertisement
+     * @param target IP Address of target ASN
+     */
     public RCU(int Source, int NextHop, int HopType, long Cost, int RTT_Update, InetAddress target) {
 
         setRCID(Source);
@@ -33,74 +46,144 @@ public class RCU implements Serializable {
         }
     }
 
+    /**
+     * Gets the Address Book advertised by ASN
+     * @return Array of ASN known by sender
+     */
      public ASN[] getAd(){
         return advertisement;
     }
 
+    /**
+     * Returns RCID of sending ASN
+     * @return int RC ID
+     */
     public int getRCID() {
         return RCID;
     }
 
+    /**
+     * Updates RCU Sending RC ID value
+     * @param RCID New int RC ID
+     */
     public void setRCID(int RCID) {
         this.RCID = RCID;
     }
 
+    /**
+     * Return ASNID of Target ASN
+     * @return ASNID of Target ASN
+     */
     public int getLinkID() {
         return LinkID;
     }
 
+    /**
+     * Set Target ASN ID for RCU Packet
+     * @param linkID ASNID of Target Network
+     */
     public void setLinkID(int linkID) {
         LinkID = linkID;
     }
 
+    /**
+     * Return Link Type
+     * @return 1 if ASN with no RC, 2 if ASN with RC
+     */
     public int getLinkType() {
         return LinkType;
     }
 
+    /**
+     * Set the Link Type
+     * @param linkType int representing the linkType. 1 if ASN without RC, 2 if ASN with RC
+     */
     public void setLinkType(int linkType) {
         LinkType = linkType;
     }
 
+    /**
+     * Returns Link Cost as a long
+     * @return long LinkCost
+     */
     public long getLinkCost() {
         return LinkCost;
     }
 
+    /**
+     * Set RCU Link Cost
+     * @param linkCost new long LinkCost value
+     */
     public void setLinkCost(long linkCost) {
         LinkCost = linkCost;
     }
 
+    /**
+     * Returns RttFlag
+     * @return 0 if RCU Advertisement, 1 if RTT_REQ, 2 if RTT_RESP
+     */
     public int getRttFlag() {
         return RttFlag;
     }
 
+    /**
+     * Sets RttFlag to desired value. 0 for RCU Advertisement, 1 for RTT_REQ, 2 for RTT_RESP
+     * @param rttFlag int from 0 to 2
+     */
     public void setRttFlag(int rttFlag) {
         RttFlag = rttFlag;
     }
 
+    /**
+     * Return Time when RTT was Sent
+     * @return time when RTT_REQ was sent
+     */
     public long getRttSent() {
         return RttSent;
     }
 
+    /**
+     * Sets the RttSent value to current millisecond
+     */
     public void setRttSent() {
         RttSent = System.currentTimeMillis();
     }
 
+    /**
+     * Returns RTT Receive Time
+     * @return long time when RTT is received
+     */
     public long getRttReceived() {
         return RttReceived;
     }
 
+    /**
+     * Set RttReceive time to current millisecond
+     */
     public void setRttReceived() {
         RttReceived = System.currentTimeMillis();
     }
 
+    /**
+     * Returns RCU destination ASN InetAddress
+     * @return
+     */
     public InetAddress getTargetIP() {
         return targetIP;
     }
 
+    /**
+     * Set destination InetAddress for packet
+     * @param targetIP InetAddress of target ASN
+     */
     public void setTargetIP(InetAddress targetIP) {
         this.targetIP = targetIP;
     }
-//Package Methods
+
+    /**
+     * Returns the Round Trip Time
+     * @return long Round Trip Time, -1 if RCU is an Advertisement update
+     */
     public long getRoundTripTime()
     {
         if (getRttFlag() != 0) {
